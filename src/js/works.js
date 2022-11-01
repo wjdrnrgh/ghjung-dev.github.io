b042c378842b124a0c6528183913fc7ffc5bccf1
wjdrnrgh <wjdrnrgh1014@gmail.com>
2022-11-01 22:20:43 +0900

    works section 작업 및 미디어 쿼리 수정, background video 교체, 스크롤바 커스텀

--- src/js/works.js
+++ src/js/works.js
@@ -7,6 +7,12 @@ const worksInput4 = document.getElementById("works4");
 const worksInput5 = document.getElementById("works5");
 const inputArr = document.querySelectorAll("input[type=radio]");
 
+const firstCard = document.getElementById("portfolioCard");
+const cardFrontBtn = document.querySelectorAll(".frontBtn");
+const cardBackBtn = document.querySelectorAll(".backBtn");
+
+let cardActiveCheck = null;
+
 let worksInput1Status = null;
 let worksInput2Status = null;
 let worksInput3Status = null;
@@ -23,42 +29,91 @@ const statusCheck = () => {
 
 const handleNextBtn = () => {
   statusCheck();
+  let nextInput = null;
   if (worksInput1Status) {
     worksInput2.checked = true;
+    nextInput = worksInput2;
   }
   if (worksInput2Status) {
     worksInput3.checked = true;
+    nextInput = worksInput3;
   }
   if (worksInput3Status) {
     worksInput4.checked = true;
+    nextInput = worksInput4;
   }
   if (worksInput4Status) {
     worksInput5.checked = true;
+    nextInput = worksInput5;
   }
   if (worksInput5Status) {
     worksInput1.checked = true;
+    nextInput = worksInput1;
   }
+  handleCardTitle(null, nextInput);
 };
 const handlePrevBtn = () => {
   statusCheck();
+  let preInput = null;
   if (worksInput1Status) {
     worksInput5.checked = true;
+    preInput = worksInput5;
   }
   if (worksInput2Status) {
     worksInput1.checked = true;
+    preInput = worksInput1;
   }
   if (worksInput3Status) {
     worksInput2.checked = true;
+    preInput = worksInput2;
   }
   if (worksInput4Status) {
     worksInput3.checked = true;
+    preInput = worksInput3;
   }
   if (worksInput5Status) {
     worksInput4.checked = true;
+    preInput = worksInput4;
   }
+  handleCardTitle(null, preInput);
+};
+
+const handleFlipActive = (event) => {
+  event.target.parentElement.parentElement.parentElement.parentElement.style.transform =
+    "rotateY(-180deg)";
+};
+const handleFlipActiveN = (event) => {
+  event.target.parentElement.parentElement.parentElement.parentElement.style.transform =
+    "";
+};
+
+const handleCardTitle = (event, item) => {
+  let target = null;
+  if (!cardActiveCheck) {
+    cardActiveCheck = firstCard.querySelector(".front_content");
+    cardActiveCheck.classList.remove("card_active");
+  } else {
+    cardActiveCheck.classList.remove("card_active");
+  }
+  if (!event) {
+    target = item.labels[0].querySelector(".front_content");
+  } else {
+    target = event.target.labels[0].querySelector(".front_content");
+  }
+  target.classList.add("card_active");
+  cardActiveCheck = target;
 };
 
 statusCheck();
 
 nextBtn.addEventListener("click", handleNextBtn);
 prevBtn.addEventListener("click", handlePrevBtn);
+cardFrontBtn.forEach((item) => {
+  item.addEventListener("click", handleFlipActive);
+});
+cardBackBtn.forEach((item) => {
+  item.addEventListener("click", handleFlipActiveN);
+});
+inputArr.forEach((item) => {
+  item.addEventListener("change", handleCardTitle);
+});
